5ef9b289cec14ac54670d7c30554783c9402d3d8
donovan1997 <cesarpr1104@gmail.com>
2024-10-29 21:01:39 -0500

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const searchBar = document.getElementById('search-bar');
+    const nomSearchBar = document.getElementById('nom-search-bar');
+    const pdfViewer = document.getElementById('pdf-view[_{{{CITATION{{{_1{](https://github.com/la9una/web/tree/ba1073ae044ebb7b538a3b13f0f9598f7c410bb6/docs%2Fbootstrap%2Falignci.md)
